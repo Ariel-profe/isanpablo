f71c916b96a35b08ec4b65f5ebfbabfd3afc3c26
Ariel Elias <eliasariel@live.com>
2025-10-09 15:44:35 -0300

    feat como-estudiar component

--- app/niveles/primaria/page.tsx
+++ app/niveles/primaria/page.tsx
@@ -1,3 +1,4 @@
+import { EstudiarMejor } from '@/components/estudiar-mejor';
 import { BlurTitle } from '@/components/ui/blur-title';
 import { Button } from '@/components/ui/button';
 import { CarouselPage } from '@/components/ui/carousel-page';
@@ -12,16 +13,17 @@ const PrimariaPage = () => {
         <BlurTitle word="Nivel Primario" />
         <div className='mt-10'>
         <CarouselPage images={primaria.images}/>
-            <div className='px-3'>
-                <p className='text-xl md:text-2xl text-primary-dark-gray mt-12 text-justify'>{primaria.description}</p>
+            <div className=''>
+                <p className='text-lg text-primary-dark-gray mt-12 text-justify'>{primaria.description}</p>
                 <div className='flex flex-col mt-12 text-lg text-primary-blue uppercase'>
                     <span>{primaria.range}</span>
                     <span>{primaria.turns}</span>
                 </div>
             </div>
-            <div className='my-12 px-3'>
+            <EstudiarMejor />
+            <div className='my-12 '>
                 <h3 className='text-2xl md:text-3xl text-primary-blue uppercase underline'>horarios de consulta</h3>
-                <p className='text-lg md:text-xl text-primary-dark-gray my-5 text-justify'>Para una mejor organización y atención de calidad en las consultas, tanto de docentes como de directivos, se recomienda a los padres solicitar con anticipación sus entrevistas y esperar la confirmación de la misma.</p>
+                <p className='text-lg text-primary-dark-gray my-5 text-justify'>Para una mejor organización y atención de calidad en las consultas, tanto de docentes como de directivos, se recomienda a los padres solicitar con anticipación sus entrevistas y esperar la confirmación de la misma.</p>
                 <p className='text-xl text-primary-dark-gray my-5 italic'>Los horarios de consultas se deben ver con docentes de aula y especiales.</p>
                 <span className='text-lg text-primary-red'>En caso de URGENCIAS consultar a dirección.</span>
             </div>
---
+++ components/estudiar-mejor.tsx
@@ -0,0 +1,30 @@
+
+export const EstudiarMejor = () => {
+    return (
+        <div className='my-8'>
+            <h3 className='text-2xl md:text-3xl text-primary-blue uppercase underline'>A organizarse</h3>
+
+
+            <div className='mt-5 flex flex-col lg:flex-row items-start justify-between'>
+                <div className='text-lg text-primary-dark-gray'>
+                    <p>
+                        Al momento de estudiar es necesario saber que cada uno de nosotros tiene diferentes modos de aprender, por lo tanto es importante que podamos conocernos a nosotros mismos y ver si las herramientas que utilizamos nos sirven o debemos buscar nuevas maneras para aprender.
+                    </p>
+                    <h5 className='mt-3'>Conozcamos cómo aprendemos y qué estrategias nos sirven en cada momento:</h5>
+                </div>
+                <img src="/assets/global/estudiar-mejor.png" alt="estudiar-mejor" />
+            </div>
+
+            <div className='mt-5 text-lg text-primary-dark-gray'>
+                <ul className='list-disc list-inside mt-3 space-y-2 text-justify'>
+                    <li>Organiza tu tiempo y materiales para evitar pérdida de motivación. Usa tu calendario Google para agendar tareas y evaluaciones.</li>
+                    <li>Estudia lejos del celular, TV o radio, te permitirá sostener la atención en el tiempo sin distracciones.</li>
+                    <li>Al momento de sentarse a estudiar comienza con lo más difícil y termina con lo más fácil, ya que al principio es más rápida la incorporación de información porque estamos con más energía.</li>
+                    <li>Comienza estudiando 30 minutos e incorpora por semana 15 min. hasta llegar a las 2 hs. Es cuestión de entrenamiento como un deporte.</li>
+                    <li>No te olvides de descansar, es importante que en esos momentos nuestra cabeza se despeje salí a caminar, hace un deporte, come algo. No se recomienda quedarnos en la computadora porque así continuas haciendo un esfuerzo mental y no descansas.</li>
+                    <li>Revisa Classroom y GEM para conocer tus calificaciones.</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
